ae46c95bd20ee2e6f58af485ee4ee17e5deb0512
Andreas Hauenstein <andreas.hauenstein@protxx.com>
2020-02-25 12:01:21 -0800

    new net 8302321

--- static/main.js
+++ static/main.js
@@ -376,7 +376,7 @@ function main( JGO, axutil, p_options) {
     const bots = BOTS
 
     const images = ['static/leela.png','static/fry.png', 'static/bender.png', 'static/farnsworth.png']
-    const names = ['Leela c1a5dea','Fry', 'Bender', 'Prof. Farnsworth']
+    const names = ['Leela 8302321','Fry', 'Bender', 'Prof. Farnsworth']
     const strengths = ['9P', 'Oh well', 'Not bad', '6D' ]
 
     var idx = 0
